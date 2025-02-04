efc1692a55c835d240a67da7f3f97db358b8b79b
Said Rodrigues <coderflemis@gmail.com>
2025-02-04 14:42:36 -0300

    feat: add base cli lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod cli;
 mod complete_command;
 mod error;
 pub mod fs;
@@ -6,18 +7,3 @@ pub mod repl;
 pub mod repl_v2;
 pub mod system;
 pub mod vdisk;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
