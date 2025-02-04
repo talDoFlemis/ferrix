f63b9bdc5120095eaf71c5d1a739dea37167b5c1
Said Rodrigues <coderflemis@gmail.com>
2025-02-04 14:50:33 -0300

    feat: add basicfs implementation

--- src/fs.rs
+++ src/fs.rs
@@ -1,1 +1,15 @@
+use crate::vdisk::VDisk;
+
 pub trait Filesystem {}
+
+pub struct BasicFS {
+    vdisk: VDisk,
+}
+
+impl BasicFS {
+    pub fn new(vdisk: VDisk) -> Self {
+        Self { vdisk }
+    }
+}
+
+impl Filesystem for BasicFS {}
